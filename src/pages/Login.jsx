3029f093b7bf3bce84a7c264db09904ed1df46bb
Sharon Florez <shdflorezs35@gmail.com>
2023-09-25 15:31:22 -0500

    Add Formik and Yup to login and register forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,44 +1,91 @@
-import { useState } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { NavLink } from "react-router-dom";
 import { login } from "../config/firebase";
 import { useUserContext } from "../context/UserContext";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const { user } = useUserContext();
   useRedirectActiveUser(user, "/dashboard");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (
+    { email, password },
+    { setSubmitting, setErrors, resetForm }
+  ) => {
     try {
       await login({ email, password });
+      resetForm();
     } catch (error) {
       console.log(error);
+      error.code === "auth/invalid-login-credentials" &&
+        setErrors({
+          email: "El usuario y/o contraseña son inválidos",
+          password: "El usuario y/o contraseña son inválidos",
+        });
+    } finally {
+      setSubmitting(false);
     }
   };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Email no válido").required("Email requerido"),
+    password: Yup.string()
+      .trim()
+      .min(6, "Mínimo 6 caráteres")
+      .required("Password requerida"),
+  });
 
   return (
     <div className="p-3">
       <h1>Login</h1>
-      <form className="d-flex gap-2 flex-column" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Ingrese email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Ingrese contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn btn-warning" type="submit">
-          Login
-        </button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          handleSubmit,
+          handleChange,
+          errors,
+          touched,
+          handleBlur,
+          isSubmitting,
+        }) => (
+          <form className="d-flex gap-2 flex-column" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Ingrese email"
+              value={values.email}
+              onChange={handleChange}
+              name="email"
+              onBlur={handleBlur}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              placeholder="Ingrese contraseña"
+              value={values.password}
+              onChange={handleChange}
+              name="password"
+              onBlur={handleBlur}
+            />
+            {errors.password && touched.password && errors.password}
+            <button
+              className="btn btn-warning"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Login
+            </button>
+
+            <NavLink className="my-4" to="/register">
+              Registrarme
+            </NavLink>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,44 +1,90 @@
-import { useState } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { NavLink } from "react-router-dom";
 import { register } from "../config/firebase";
 import { useUserContext } from "../context/UserContext";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const { user } = useUserContext();
   useRedirectActiveUser(user, "/dashboard");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (
+    { email, password },
+    { setSubmitting, setErrors, resetForm }
+  ) => {
     try {
       await register({ email, password });
+      resetForm();
     } catch (error) {
       console.log(error);
+      error.code === "auth/email-already-in-use" &&
+        setErrors({
+          email: "Este correo ya está registrado.",
+        });
+    } finally {
+      setSubmitting(false);
     }
   };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Email no válido").required("Email requerido"),
+    password: Yup.string()
+      .trim()
+      .min(6, "Mínimo 6 caráteres")
+      .required("Password requerida"),
+  });
 
   return (
     <div className="p-3">
       <h1>Register</h1>
-      <form className="d-flex gap-2 flex-column" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Ingrese email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Ingrese contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn btn-warning" type="submit">
-          Register
-        </button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          handleSubmit,
+          handleChange,
+          errors,
+          touched,
+          handleBlur,
+          isSubmitting,
+        }) => (
+          <form className="d-flex gap-2 flex-column" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Ingrese email"
+              value={values.email}
+              onChange={handleChange}
+              name="email"
+              onBlur={handleBlur}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              placeholder="Ingrese contraseña"
+              value={values.password}
+              onChange={handleChange}
+              name="password"
+              onBlur={handleBlur}
+            />
+            {errors.password && touched.password && errors.password}
+            <button
+              className="btn btn-warning"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Register
+            </button>
+            <p className="my-4">
+              ¿Ya tienes una cuenta?
+              <NavLink to="/">Inicia sesión</NavLink>
+            </p>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
